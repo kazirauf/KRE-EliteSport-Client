5353e04bcfaa17ee0772b3851051ca6b09c3f88d
kazirauf <raufelahi9876@gmail.com>
2024-08-30 01:01:14 +0600

    contact us is done fully with functional

--- src/Pages/ContactUs.tsx
+++ src/Pages/ContactUs.tsx
@@ -1,119 +1,69 @@
+import Swal from 'sweetalert2'
+import '../index.css'
 const ContactUs = () => {
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "06dad70c-7739-42a4-a0b3-5427272b2bae");
+
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+
+    const res = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: json
+    }).then((res) => res.json());
+
+    if (res.success) {
+        Swal.fire({
+            title: "Your Message is send!",
+            icon: "success"
+          });
+    }
+  };
   return (
-    <div className="bg-[#F5EDED]">
-      <form>
-        <section className="text-gray-600 body-font relative">
-          <div className="container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap">
-            <div className="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
-              <iframe
-                width="100%"
-                height="100%"
-                className="absolute inset-0"
-                title="map"
-                src="https://maps.google.com/maps?width=100%&height=600&hl=en&q=Dhaka+(My%20Business%20Name)&ie=UTF8&t=&z=14&iwloc=B&output=embed"
-                style={{ filter: "grayscale(0.3) contrast(1.2) opacity(0.4)" }}
-              ></iframe>
-              <div className="bg-white relative flex flex-wrap py-6 rounded shadow-md">
-                <div className="lg:w-1/2 px-6">
-                  <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">
-                    ADDRESS
-                  </h2>
-                  <p className="mt-1">
-                    60 feet road, barek molla more, Mirpur 2, Dhaka
-                  </p>
-                </div>
-                <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
-                  <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">
-                    EMAIL
-                  </h2>
-                  <a className="text-red-500 leading-relaxed">
-                    hi@sportease.com
-                  </a>
-                  <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs mt-4">
-                    PHONE
-                  </h2>
-                  <p className="leading-relaxed">123-456-7890</p>
-                </div>
-              </div>
+    <div id="contact" className="my-20">
+       <h1 className="text-center text-4xl font-bold mt-20">Contact <span className="text-blue-500">Us</span></h1>
+
+    <div className="flex justify-center">
+        <div className="flex flex-col md:flex-row items-center 2xl::ml-52 text-white p-6">
+            <div className="md:w-1/2 p-4">
+                <img src="https://images.unsplash.com/photo-1515378960530-7c0da6231fb1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Profile" className="rounded-md lg:max-w-xl max-w-sm  rounded-lg shadow-2xl" />
             </div>
-            <div className="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 px-7 mt-8 md:mt-0">
-              <h2 className="text-gray-900 text-2xl mb-1 font-bold title-font">
-                Contact us
-              </h2>
-              <p className="leading-relaxed mb-5 text-gray-600">
-                If you have any doubts, please contact us by submitting
-                following form
-              </p>
-
-              <div className="relative mb-4">
-                <label
-                  htmlFor="name"
-                  className="leading-7 text-sm text-gray-600"
-                >
-                  Name
-                </label>
-                <input
-                  type="text"
-                  id="name"
-                  name="name"
-                  className="w-full bg-white rounded border border-gray-300 focus:border-button focus:ring-2 focus:ring-button text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                />
-              </div>
-
-              <div className="relative mb-4">
-                <label
-                  htmlFor="email"
-                  className="leading-7 text-sm text-gray-600"
-                >
-                  Email
-                </label>
-                <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  className="w-full bg-white rounded border border-gray-300 focus:border-button focus:ring-2 focus:ring-button text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                />
-              </div>
-
-              <div className="relative mb-4">
-                <label
-                  htmlFor="email"
-                  className="leading-7 text-sm text-gray-600"
-                >
-                  Subject
-                </label>
-                <input
-                  type="text"
-                  id="email"
-                  name="email"
-                  className="w-full bg-white rounded border border-gray-300 focus:border-button focus:ring-2 focus:ring-button text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                />
-              </div>
-
-              <div className="relative mb-4">
-                <label
-                  htmlFor="message"
-                  className="leading-7 text-sm text-gray-600"
-                >
-                  Message
-                </label>
-                <textarea
-                  id="message"
-                  name="message"
-                  className="w-full bg-white rounded border border-gray-300 focus:border-button focus:ring-2 focus:ring-button text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                ></textarea>
-              </div>
-              <button
-                type="button"
-                className="text-white bg-button border-0 py-2 px-6 focus:outline-none hover:bg-button-dark rounded text-lg"
-              >
-                Submit
-              </button>
+            <div className="md:w-1/2 p-4">
+                <form  onSubmit={onSubmit} className="space-y-4">
+                    <input
+                        type="text"
+                        name="name"
+                        placeholder="Your Name"
+                        className="w-full p-3 bg-gray-800 text-white rounded-md focus:outline-none focus:ring-2 focus:ring-gray-400"
+                    />
+                    <input
+                        type="email"
+                        placeholder="E-mail"
+                        name="email"
+                        className="w-full p-3 bg-gray-800 text-white rounded-md focus:outline-none focus:ring-2 focus:ring-gray-400"
+                    />
+                    <textarea
+                        placeholder="Your Message"
+                        name="message"
+                        className="w-full p-3 h-32 bg-gray-800 text-white rounded-md focus:outline-none focus:ring-2 focus:ring-gray-400"
+                    ></textarea>
+                    <input
+                        className="w-full p-3 bg-black text-white rounded-md hover:bg-white hover:text-black hover:border-2 hover:border-black transition duration-200"
+                        type="submit"
+                        value="Send"
+                    />
+                </form>
             </div>
-          </div>
-        </section>
-      </form>
+        </div>
     </div>
+</div>
   );
 };
 
